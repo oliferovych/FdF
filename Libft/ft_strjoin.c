3fc9cbd878b568cf87eb18327bbfca88ee1b4983
oliferovych <dimasikolif@gmail.com>
2024-08-10 22:08:00 +0200

    compilation fix

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 18:14:12 by dolifero          #+#    #+#             */
-/*   Updated: 2024/03/11 17:17:56 by dolifero         ###   ########.fr       */
+/*   Updated: 2024/08/10 21:53:36 by dolifero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,10 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int		i;
-	int		j;
 	char	*concatenated;
 	int		s1_len;
 	int		s2_len;
 
-	i = 0;
-	j = 0;
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	concatenated = malloc(s1_len + s2_len + 1);
